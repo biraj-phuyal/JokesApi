90971b6039ecf376ca4266cf17f2ace26360e7ab
biraj-phuyal <birajphuyal11@gmail.com>
2025-04-14 14:05:54 +0100

    completed controller

--- app/controllers/jokes_controller.rb
+++ app/controllers/jokes_controller.rb
@@ -3,27 +3,27 @@
 # this is a class of jokes
 class JokesController < ApplicationController
   def index
-    @jokes = Joke.all
-    render json: @joke
+    jokes = Joke.all
+    render json: jokes
   end
 
   def show
-    @joke = Joke.find_by(id: params[:id])
-    render json: @joke
+    joke = Joke.find_by(id: params[:id])
+    render json: joke
   end
 
   def create
-    @joke = Joke.create(jokes_params)
+    joke = Joke.create(jokes_params)
     if joke.save
-      render json: { message: 'A joke has been created' }, status: :success
+      render json: joke, status: :created
     else
-      render json: { message: 'Failed to create the joke' }, status: :unsuccessful
+      render json: { message: 'Failed to create the joke', erros: joke.errors }, status: :unprocessable_entity
     end
   end
 
   private
 
   def jokes_params
-    require(:joke).permit(:content, :homour, :context)
+    params.require(:joke).permit(:content, :humour, :context)
   end
 end
