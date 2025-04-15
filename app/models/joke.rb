e0dd42f375ea15d620ae4fa11ce748e5b7eb8f8b
biraj phuyal <birajphuyal@birajs-MacBook-Pro.local>
2025-04-15 11:05:06 +0100

    added some comments

--- app/models/joke.rb
+++ app/models/joke.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
+# Joke class
 class Joke < ApplicationRecord
-    validates :content, presence: true
-    validates :humour, presence: true
-    validates :context, presence: true
+  validates :content, presence: true
+  validates :humour, presence: true
+  validates :context, presence: true
 end
